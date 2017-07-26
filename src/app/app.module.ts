68b252bd04d9e465027292f2db09e2485d154d3e
Amine Ghilazghi <aghilazghi@yahoo.com>
2017-07-26 11:09:07 -0400

    Completed generating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MembersComponent } from './components/members/members.component';
+import { MemberDetailsComponent } from './components/member-details/member-details.component';
+import { AddMemberComponent } from './components/add-member/add-member.component';
+import { EditMemberComponent } from './components/edit-member/edit-member.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingComponent } from './components/setting/setting.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MembersComponent,
+    MemberDetailsComponent,
+    AddMemberComponent,
+    EditMemberComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule
